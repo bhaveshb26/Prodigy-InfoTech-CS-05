e0232a409d63ff40e45f4402d0e36a1f368e92ae
Bhavesh Arvind Bhadane <bbhadane2605@gmail.com>
2025-07-01 00:52:43 +0530

    Delete packet_sniffer.py

--- packet_sniffer.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import sniff
-
-def packet_callback(packet):
-    print(packet.summary())
-
-print("Starting packet capture... (Ctrl+C to stop)")
-sniff(prn=packet_callback, count=10)
